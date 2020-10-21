fc1f0124d09636065bd42f599bac74aad4c6da46
Andrew Issac <aissac@ekplx42.ekp.kit.edu>
2020-10-21 03:28:47 +0200

    First commit

---
+++ MLConfig.py
@@ -0,0 +1,49 @@
+# region ######### Classes ######### 
+
+from enum import Enum, unique
+# If needed to compare Enum with int -> use IntEnum
+# Category.name -> z.B. Category.GenuineTau.name == 'GenuineTau'
+# Category.value -> z.B. Category.GenuineTau.value == 0.0
+@unique
+class MLCategory(Enum):
+    GenuineTau = 0.0
+    FakeTau = 1.0
+
+@unique
+class TBranches(Enum): # these are branches in TTrees
+    Tau_pt = 0.0
+    Tau_eta = 1.0
+    Tau_phi = 2.0
+    Tau_mass = 3.0
+    Tau_dxy = 4.0
+    Tau_decayMode = 5.0
+    Tau_ecalEnergy = 6.0
+    Tau_hcalEnergy = 7.0
+    Tau_ip3d = 8.0
+
+# endregion ######### Classes ######### 
+
+
+# region ######### Global stuff ######### 
+
+# region ######### Paths and files ######### 
+from os import path
+basepath = '/work/aissac/'
+testData_basepath = path.join(basepath, 'TestRootFiles')
+
+fileDic = {
+    MLCategory.GenuineTau : ['testDYJetsToLL.root'],
+    MLCategory.FakeTau : ['testWJetsToLNu.root']
+}
+# endregion ######### Paths and files ######### 
+
+# region ######### ML Lists ######### 
+# get names (e.g. for a dict) by using: ml_variables[myIndex].name
+# get value by using: ml_variables[myIndex].value
+# ml_variables are values stored in branches from TTree
+ml_variables = [member for name, member in TBranches.__members__.items()]
+ml_categories = [member for name, member in MLCategory.__members__.items()]
+# endregion ######### ML Lists ######### 
+
+# endregion ######### Global stuff ######### 
+
---
+++ train.py
@@ -0,0 +1,112 @@
+
+import uproot4
+import sys
+import argparse
+import copy
+#import ROOT
+import tensorflow as tf
+import matplotlib.pyplot as plt 
+import matplotlib
+import numpy as np
+import pickle
+import MLConfig as cfg
+from os import path
+from sklearn.utils import shuffle
+
+print("\n########## BEGIN PYTHON SCRIPT ############")
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("-f", "--filename", required=True, type=str)
+# args = parser.parse_args()
+
+# region ######### Methods ######### 
+# args must be numpy arrays + have the same length!
+# but somehow this does not change the arrays outside the function although numpy arrays are mutable ??
+def shuffleNumpyArrays(*args):
+    try:
+        indices_ = np.arange(args[0].shape[0])
+        np.random.shuffle(indices_)
+        for arr in args:
+            arr = arr[indices_]
+    except Exception as ex:
+        print("shuffleNumpyArrays() - ", ex)
+
+def shuffleArrays(*args):
+    return shuffle(args, random_state = 0)
+
+def rootTTree2numpy(path_, rootFile):
+    f = uproot4.open(path.join(path_, rootFile))
+    ttree = f['tauEDAnalyzer']['Events'] # TTree name: 'Events'
+    return ttree.arrays(library="np")
+
+def buildDataset(path_, fileDic, branchesToGetFromRootFiles, encodelabels_OneHot = True):
+    inputs_ = [] 
+    labels_ = []
+    branchNames = [branch.name for branch in branchesToGetFromRootFiles]
+    for category, rootFilesList in fileDic.items(): # category is the key and rootFiles is the value
+        for rootFile in rootFilesList:
+            tree = rootTTree2numpy(path_, rootFile)
+            entryCount = tree[branchNames[0]].shape[0]
+            # ml_variable.name is the branch name of the TTree. Each branch has one float variable e.g. Tau_pt
+            inputs_.append(np.vstack([np.array(tree[branch], dtype=np.float32) for branch in branchNames]).T)
+            labels_.append(np.full((entryCount,), category.value, dtype=np.float32))
+
+    # Stack all inputs_ vertically
+    inputs_ = np.vstack(inputs_)
+
+    # Stack all labels_ horizontally
+    labels_ = np.hstack(labels_)
+
+    if(encodelabels_OneHot):
+        labels_ = tf.keras.utils.to_categorical(labels_)
+
+    # Shuffle inputs_/labels_
+    indices = np.arange(labels_.shape[0])
+    np.random.shuffle(indices)
+    inputs_ = inputs_[indices]
+    labels_ = labels_[indices]
+    
+    return inputs_, labels_
+# endregion ######### Methods ######### 
+
+# region ######### get dataset from root files with uproot4 ######### 
+# ml_variables are values stored in branches from TTree
+inputs, labels = buildDataset(cfg.testData_basepath, cfg.fileDic, cfg.ml_variables)
+validationEntryCount = -10000
+inputs_validation = inputs[validationEntryCount:]
+labels_validation = labels[validationEntryCount:]
+inputs_train = inputs[:validationEntryCount]
+label_train = inputs[:validationEntryCount]
+# endregion ######### get data from root files with uproot4 ######### 
+
+
+# region ######### Tensorflow / Keras ######### 
+# region ######### NN Model ######### 
+# NN with 9 Input Nodes, 100 hidden nodes and 1 Output Node (Probability of being Signal/background)
+model = tf.keras.Sequential()
+model.add(tf.keras.Input(shape=(len(cfg.ml_variables),), name="inputLayer"))
+model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu, input_shape=(len(cfg.ml_variables),), name="dense_1"))
+model.add(tf.keras.layers.Dense(32, activation=tf.nn.relu, input_shape=(len(cfg.ml_variables),), name="dense_2"))
+model.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax, name="predictions"))
+
+model.summary()
+# endregion ######### NN Model ######### 
+
+loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+
+history = model.fit(
+    inputs, 
+    labels, 
+    batch_size=100,
+    epochs=10,
+    validation_data=(inputs_validation, labels_validation)
+)
+
+print("\n")
+print(history.history)
+# endregion ######### Tensorflow / Keras ######### 
+
+
+print("########## END PYTHON SCRIPT ############\n")
